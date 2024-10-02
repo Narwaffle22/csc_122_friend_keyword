ef401ed21702496a77b9c4d52e4a4f1ab113f957
Narwaffle22 <113711372+Narwaffle22@users.noreply.github.com>
2024-10-02 16:44:02 -0500

    Update 3_friend_classes.cpp

--- 3_friend_classes.cpp
+++ 3_friend_classes.cpp
@@ -3,42 +3,47 @@
 using namespace std;
 
 class GeometricScaler;
+class GeometricSwapper;
 
-class Rectangle
-{
+class Rectangle {
   int width, height;
-
 public:
   Rectangle(int w, int h) : width(w), height(h) {}
-
-  int area()
-  {
+  int area() {
     return (width * height);
   }
   void scale(GeometricScaler scalar);
+  void swap(GeometricSwapper swapper);
 };
 
-class GeometricScaler
-{
+class GeometricScaler {
 private:
   friend class Rectangle;
   int factor;
-
 public:
-  GeometricScaler(int factor)
-  {
+  GeometricScaler(int factor) {
     this->factor = factor;
   }
 };
-
-void Rectangle::scale(GeometricScaler scalar)
-{
+void Rectangle::scale(GeometricScaler scalar) {
   this->width = this->width * scalar.factor;
   this->height = this->height * scalar.factor;
 }
 
-int main()
-{
+class GeometricSwapper {
+private:
+  friend class Rectangle;
+public:
+
+};
+void Rectangle::swap(GeometricSwapper swapper) {
+  int temp = this->width;
+  int jemp = this->height;
+  this->width = jemp;
+  this->height = temp;
+}
+
+int main() {
   Rectangle rectangle(1, 2);
   cout << "Area of the Rectangle: " << rectangle.area() << endl;
 
